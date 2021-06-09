8feef5a0323a55982307f35d68719d83eac7c42d
Japanotaku <gotovitsakentocenzestko@gmail.com>
2021-06-10 04:15:36 +0600

    Python_new_level

--- aut.py
+++ aut.py
@@ -1,3 +1,8 @@
+
+from sqlite3.dbapi2 import Error
+import sqlite3
+connect = sqlite3.connect('test.db')
+
 def query_func(db_name, table_name, col_name, query_value):
     import sqlite3
     conn = sqlite3.connect(db_name)
@@ -47,25 +52,49 @@ def query_func2(db_name, table_name, col_name1,col_name2,query_value):
 
 
 
+def delete_user(con,lgn):
+	try:
+		cursorObj = con.cursor()
+		cursorObj.execute('DELETE FROM users WHERE login = ?', (lgn,))
+		con.commit()
+	except Error:
+		print("Hey,you are bad guy", Error)
+	finally:
+	     if cursorObj:
+							cursorObj.close()
 
 scr_lgn = input("Login: ")
 
 
+Warnings = ["Wrong password, You have two attempts left", "Wrong password, You have one attempts left", "Wrong password, You are blocked"]
 
-
-
+flag = True
 
 lgn = query_func('test.db','users','login',scr_lgn)
 
+
+
 if (scr_lgn != lgn) or (len(scr_lgn) == 0):
-    print ("Incorrect login")
-else:
+	while (scr_lgn != lgn):
+					scr_lgn = input("Login: ")
+					lgn = query_func('test.db','users','login',scr_lgn)
+
+if(scr_lgn == lgn):
     scr_psw = input("Password: ")
     psw = query_func('test.db','users','password',hash_md5_func(scr_psw))
 
     if (hash_md5_func(scr_psw) != psw) or (len(scr_psw) == 0):
-        print ("Wrong password, You have two attempts left")
-    else:
+        while(hash_md5_func(scr_psw) != psw and flag == True):
+			        for i in Warnings:
+												print(i)
+												if(i == "Wrong password, You are blocked"):
+																delete_user(connect,lgn)
+																flag = False
+														   
+												if(i != "Wrong password, You are blocked" ):		  
+													scr_psw = input("Password: ")
+													psw = query_func('test.db','users','password',hash_md5_func(scr_psw))												
+    if(hash_md5_func(scr_psw) == psw):
         surname = query_func2('test.db', 'users', 'surname','login',lgn)
         name = query_func2('test.db', 'users', 'name','login',lgn)
         print("Hello,"+ crypt_xor_func(surname,len(lgn)) + " " + crypt_xor_func(name,len(lgn)))
--- reg.py
+++ reg.py
@@ -1,48 +1,17 @@
-def check_login_func(db_name, table_name, login):
-	import sqlite3
-	conn = sqlite3.connect(db_name)
-	cursor = conn.cursor()
-	check_query = ('SELECT login FROM ' + table_name + ' WHERE login = \"' + login +"\"")
-	cursor.execute(check_query)
-	lgn = cursor.fetchone()
-	conn.close()
-	lgn = str(lgn)
-	lgn = lgn[2:len(lgn)-3]
-	
-	if (lgn == login):
-		result = 'EXIST'
-	else:
-		result = 'FREE'
-	
-	return result
-	
+action = str(input("""                                 Choose your actions: ٩(｡•́‿•̀｡)۶
+			|	1) Authorise(A)                    |
+			|	2) Register(R)                     |
+			|	3) Quit the programme(Q)           |
+			|	4) Change the login to another (T) |
+			
+			 
+		Your choice: """))
 
-def create_record_func(db_name, table_name, login, password, name, surname, status):
-	result = '?????'
-	import sqlite3
-	conn = sqlite3.connect(db_name)
-	cursor = conn.cursor()
-	create_query = ('INSERT INTO ' + table_name + ' (login, password, name, surname, status) VALUES (\'' + login + '\', \'' + password + '\', \'' + name + '\', \'' + surname + '\', \'' + status + '\');')
-	cursor.execute(create_query)
-	conn.commit()
+import time
+import sqlite3
+rare = sqlite3.connect('test.db')
 
-	check_query = ('SELECT login FROM ' + table_name + ' WHERE login = \"' + login +"\"")
-	cursor.execute(check_query)
-	lgn = cursor.fetchone()
-	conn.close()
-	
-	lgn = str(lgn)
-	lgn = lgn[2:len(lgn)-3]
-	
-	if (lgn == login):
-		result = 'SUCCESS'
-	else:
-		result = 'FAILED'
-	
-	return result
-
-def crypt_xor_func(plaintext):
-	key = 1
+def crypt_xor_func(plaintext, key):
 	ciphertext = ''
 	for i in plaintext:
 		try:
@@ -51,23 +20,71 @@ def crypt_xor_func(plaintext):
 			ciphertext+=chr(ord(i)^key)
 			
 	return ciphertext
-	
-	print("Please, complete the sign up form")
-i = 1
-while (i <= 5):
-	scr_lgn = input("Login: ")
-	check_login = check_login_func('test.db', 'users', scr_lgn)
-	if (check_login == 'EXIST'):
-		print('Login \"' + scr_lgn + '\" is already exist. \nYou have ' + str(5 - i) + ' tries to choose another login...')
-		i = i + 1
-	else:
-		scr_psw = input("Password: ")
-		scr_name = input("Name: ")
-		crypt_scr_name = crypt_xor_func(scr_name)
-		scr_surname = input("Surname: ")
-		crypt_scr_surname = crypt_xor_func(scr_surname)
-		result = create_record_func('test.db', 'users', scr_lgn,scr_psw, crypt_scr_name, crypt_scr_surname, 'A')
-		print(result)
-		break
-	
+
+def sql_update(con,choice_new_login,choose_login):
+	asd = "UPDATE users SET login = '"
+	asd += choice_new_login
+	asd +="' WHERE login = '"
+	asd +=choose_login
+	asd +="';"
+	time.sleep(1)
+	print(1)
+	time.sleep(1)
+	print(2)
+	time.sleep(1)
+	print(3)
+	time.sleep(1)
+	print(4)
+	time.sleep(1)
+	print(5)
+	cursorObj = con.cursor()
+	cursorObj.execute(asd)
+	con.commit()
+
+def sql_update2(con,choice_new_name,choose_login):
+	asd = "UPDATE users SET name '"
+	asd += choice_new_name
+	asd +="' WHERE name = '"
+	asd +=choose_login
+	asd +="';"
+	print(asd)
+	time.sleep(1)
+	print(1)
+	time.sleep(1)
+	print(2)
+	time.sleep(1)
+	print(3)
+	time.sleep(1)
+	print(4)
+	time.sleep(1)
+	print(5)
+	cursorObj = con.cursor()
+	cursorObj.execute(asd)
+	con.commit()
+
+while (True):
+  if (action == "Q"):
+	  exit() 
+  elif (action == "T"):
+	  choose_login = input("Введите логин который хотите поменять: ")
+	  choice_new_login = input("На какой логин вы хотите поменять?: ")  
+	  sql_update(rare,choice_new_login,choose_login)
+	  print("Success")
+	  exit()
+  elif (action == "L"):
+	  choose_login = input("Введите логин который хотите поменять: ")
+	  choice_new_name = input("На какое имя вы хотите поменять?: ")  
+	  sql_update2(rare,choice_new_name,choose_login)
+	  print("Success")
+	  exit()
+  elif (action == "A"):
+	  import aut 
+	  break
+  elif (action == "R"):
+	  import reg_new 
+	  break
+  else:
+      print("Try again")
+      break
+
 	
